ad5d7c76d0d970f2ea11bd41463f5ae3179165c5
Luke Spademan <info@lukespademan.com>
2022-12-01 16:01:37 +0000

    solved day01

---
+++ src/bin/day01.rs
@@ -0,0 +1,35 @@
+use aoc2022::{read_input, solution, Args, Solution};
+use clap::Parser;
+
+fn get_elves(data: String) -> Vec<i32> {
+    let mut elves = vec![];
+    let mut elf = 0;
+    for line in data.lines() {
+        if line.is_empty() {
+            elves.push(elf);
+            elf = 0;
+        } else {
+            elf += line.parse::<i32>().unwrap();
+        }
+    }
+    elves.sort_by(|a, b| b.cmp(a));
+    elves
+}
+
+fn solve_part_a(elves: &[i32]) -> Option<i32> {
+    Some(elves[0])
+}
+
+fn solve_part_b(elves: &[i32]) -> Option<i32> {
+    Some(elves.iter().take(3).sum())
+}
+
+pub fn solve(data: String) -> Solution {
+    let elves = get_elves(data);
+    solution!(solve_part_a(&elves), solve_part_b(&elves), 1)
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("{}", solve(read_input(1, args)));
+}
---
+++ src/lib.rs
@@ -0,0 +1,67 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+pub struct Solution {
+    pub day: usize,
+    pub part_a: Option<String>,
+    pub part_b: Option<String>,
+}
+
+#[macro_export]
+macro_rules! solution {
+    ($a:expr,$b:expr, $d:expr) => {{
+        Solution {
+            part_a: match $a {
+                Some(a) => Some(a.to_string()),
+                None => None,
+            },
+            part_b: match $b {
+                Some(b) => Some(b.to_string()),
+                None => None,
+            },
+            day: $d,
+        }
+    }};
+}
+
+impl std::fmt::Display for Solution {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let part_a = match &self.part_a {
+            Some(value) => value,
+            None => "Unsolved",
+        };
+        let part_b = match &self.part_b {
+            Some(value) => value,
+            None => "Unsolved",
+        };
+        write!(
+            f,
+            "Day {}:\n  Part A: {}\n  Part B: {}",
+            self.day, part_a, part_b,
+        )
+    }
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    #[arg(short, long, default_value_t = false, help = "Use test data file")]
+    pub test_data: bool,
+    #[arg(short, long, help = "Use test data file")]
+    pub input_file: Option<PathBuf>,
+}
+
+pub fn read_input(day: usize, args: Args) -> String {
+    let file = match args.input_file {
+        Some(filename) => filename,
+        None => PathBuf::from(format!(
+            "./inputs/day{:0>2}{}.txt",
+            day,
+            match args.test_data {
+                true => "-test",
+                false => "",
+            }
+        )),
+    };
+    std::fs::read_to_string(file).unwrap()
+}
